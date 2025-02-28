42effab8cb32d03af7dd208da5ad173c0561fee2
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-02-28 17:26:01 +0000

    Add test project

---
+++ BibleReader.Tests/Usx/UsxDoc.Test.cs
@@ -0,0 +1,17 @@
+using BibleReader;
+
+namespace BibleReader.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
